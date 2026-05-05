10a31a6baa32258672e2971031ed68a91264d956
Ouhfi <ouhfid4@gmail.com>
2026-05-05 21:21:38 +0100

    start project

---
+++ beat-mawazin-2026/src/App.tsx
@@ -0,0 +1,17 @@
+import { Home } from "./components/home"
+import { Card } from "./components/Card";
+import { HeroContent } from "./components/HeroContent";
+function App() {
+  return (
+    <div className="bg-amber-700">
+      
+     
+         
+         <Home/>
+         <HeroContent/>
+        <Card/>
+        </div>
+  );
+}
+
+export default App;
---
+++ beat-mawazin-2026/src/components/HeroContent.tsx
@@ -0,0 +1,8 @@
+export function HeroContent() {
+  return (
+    <div className="text-white text-center">
+      <h1 className="text-5xl font-bold">Mawazine 2025</h1>
+      <p className="mt-4 text-xl">Welcome to the concert</p>
+    </div>
+  );
+}
