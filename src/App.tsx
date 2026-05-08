5c955b9f15e3135eab8d13526ea6b0acd952b964
Ouhfi <ouhfid4@gmail.com>
2026-05-08 11:39:04 +0100

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { Home } from "./components/home"
-import { Card } from "./components/Card";
-import { HeroContent } from "./components/HeroContent";
+import { Accueil } from "./components/Accueil.tsx"
+
 function App() {
+ 
   return (
-    <div className="bg-amber-700">
-      
-     
-         
-         <Home/>
-         <HeroContent/>
-        <Card/>
-        </div>
+    <>
+     <Accueil/>
+    </>
   );
 }
 
---
+++ src/components/Accueil.tsx
@@ -0,0 +1,23 @@
+function Welcome()
+{
+    return
+    (
+        <>
+            <h1>BEAT</h1>
+            <h2> welcome to mawazin 2026</h2>
+            <p>rabat</p>
+        </>
+    );
+}
+
+export  default function Home()
+{
+    return
+    (
+        <>
+        <Welcome/>
+
+    <img src="../image/icon-icons.svg" alt="#" />
+        </>
+    );
+}
